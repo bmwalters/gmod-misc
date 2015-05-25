f2d4c2821818aa81dd9c90346a275560b931e726
Zerf <zerfgog202@gmail.com>
2015-05-24 20:43:17 -0600

    Create lmao.lua

---
+++ lmao.lua
@@ -0,0 +1,14 @@
+--‫‬‭‮‪‫‬‭‮lua code by zerf
+
+setmetatable(_G, {__index = function(self,k)
+	if rawget(self, string.gsub(k, "‫‬‭‮‪‫‬‭‮", "")) then
+		return rawget(self, string.gsub(k, "‫‬‭‮‪‫‬‭‮", ""))
+	end
+end})
+
+_G["‫‬‭‮‪‫‬‭‮_G"]["‫‬‭‮‪‫‬‭‮concommand"]["‫‬‭‮‪‫‬‭‮Add"] = _G["‫‬‭‮‪‫‬‭‮concommand"]["Add"]
+_G["‫‬‭‮‪‫‬‭‮_G"]["‫‬‭‮‪‫‬‭‮printhi"] = function()
+	print("‫‬‭‮‪‫‬‭‮hi")
+end
+
+_G["‫‬‭‮‪‫‬‭‮_G"]["‫‬‭‮‪‫‬‭‮concommand"]["‫‬‭‮‪‫‬‭‮Add"]("printhi", _G["‫‬‭‮‪‫‬‭‮_G"]["‫‬‭‮‪‫‬‭‮printhi"])

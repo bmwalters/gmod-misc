1592b4d3ae298048c8e2fe200103fb70aa29d8be
Zerf <zerfgog202@gmail.com>
2015-05-27 16:40:41 -0600

    Update pointshopconvert.lua

--- pointshopconvert.lua
+++ pointshopconvert.lua
@@ -1,17 +1,15 @@
-local pleasedo = true
+-- Script to convert Pointshop PData or flatfile storage to your new data provider (probably SQL)
 
 local function convertpoints(ply)
-	if not pleasedo then return end
+	local filename = string.gsub(ply:SteamID(), ":", "_")
+	local plypoints = ply:GetPData("PS_Points", false)
+	local plyitems = util.JSONToTable(ply:GetPData("PS_Items", "{}"))
 
-	local filename = string.Replace(ply:SteamID(), ':', '_')
-	local plypoints = ply:GetPData('PS_Points', 0)
-	local plyitems = util.JSONToTable(ply:GetPData('PS_Items', '{}'))
-
-	if plypoints ~= 0 then
+	if plypoints then -- convert from PData
 		PS:SetPlayerData(ply, plypoints, plyitems)
-		ply:RemovePData('PS_Points')
-		ply:RemovePData('PS_Items')
-	elseif file.Exists("pointshop/"..filename..".txt", "DATA") then
+		ply:RemovePData("PS_Points")
+		ply:RemovePData("PS_Items")
+	elseif file.Exists("pointshop/"..filename..".txt", "DATA") then -- convert from flatfile
 		local data = util.JSONToTable(file.Read("pointshop/"..filename..".txt", "DATA"))
 		local plypoints = data.Points or 0
 		local plyitems = data.Items or {}
